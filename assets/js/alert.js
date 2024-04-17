60a468049df743f001a902cd26c64a2cfadaddc2
lpupmarks <149400378+lpupmarks@users.noreply.github.com>
2024-04-17 22:09:04 +0530

    update assets/js/alert.js

--- assets/js/alert.js
+++ assets/js/alert.js
@@ -2,7 +2,7 @@ function displayPopup() {
             setTimeout(function() {
                 Swal.fire({
                     title: '',
-                    html: '<br/><a href="https://rahulgogoi.vercel.app/100books.html"  target="_blank" rel="noopener norefferer">Click here</a> to track my progress.',
+                    html: 'I have taken on the challenge of reading 100 books<br/><a href="https://rahulgogoi.vercel.app/100books.html"  target="_blank" rel="noopener norefferer">Click here</a> to track my progress.',
                     icon: 'info',
                     showCloseButton: true,
                     showConfirmButton: false
