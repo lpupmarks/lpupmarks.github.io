13ba21f079487303277b8931693cc32be8d91605
lpupmarks <149400378+lpupmarks@users.noreply.github.com>
2024-04-18 10:20:01 +0530

    update assets/js/alert.js

--- assets/js/alert.js
+++ assets/js/alert.js
@@ -4,13 +4,10 @@ function displayPopup() {
                     title: '',
                     html: 'I have taken on the challenge of reading 100 books<br/><a href="https://rahulgogoi.vercel.app/100books.html"  target="_blank" rel="noopener norefferer">Click here</a> to track my progress.',
                     icon: 'info',
-                    color: "#716add",
-  background: "#fff url(https://sweetalert2.github.io/images/trees.png)",
                     showCloseButton: true,
                     showConfirmButton: false
                 });
             }, 2500); 
         }
 
-        // Call the function on page load
         window.onload = displayPopup;
