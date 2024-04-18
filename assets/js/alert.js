e80c3496ad9512e1168e67080e052a530f96ab10
lpupmarks <149400378+lpupmarks@users.noreply.github.com>
2024-04-18 10:08:01 +0530

    update assets/js/alert.js

--- assets/js/alert.js
+++ assets/js/alert.js
@@ -4,6 +4,8 @@ function displayPopup() {
                     title: '',
                     html: 'I have taken on the challenge of reading 100 books<br/><a href="https://rahulgogoi.vercel.app/100books.html"  target="_blank" rel="noopener norefferer">Click here</a> to track my progress.',
                     icon: 'info',
+                    color: "#716add",
+  background: "#fff url(https://sweetalert2.github.io/images/trees.png)",
                     showCloseButton: true,
                     showConfirmButton: false
                 });
