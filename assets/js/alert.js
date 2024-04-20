b846b4661dcafd342d9d0f6f607986bd02020407
twake <149400378+lpupmarks@users.noreply.github.com>
2024-04-20 13:42:35 +0530

    Update alert.js

--- assets/js/alert.js
+++ assets/js/alert.js
@@ -2,7 +2,7 @@ function displayPopup() {
             setTimeout(function() {
                 Swal.fire({
                     title: '',
-                    html: 'I have taken on the challenge of reading 100 books<br/><a href="https://hookedwtihbooks.vercel.app/100books.html"  target="_blank" rel="noopener norefferer">Click here</a> to track my progress.',
+                    html: 'I have taken on the challenge of reading 100 books<br/><a href="https://hookedwithbooks.vercel.app/100books.html"  target="_blank" rel="noopener norefferer">Click here</a> to track my progress.',
                     icon: 'info',
                     showCloseButton: true,
                     showConfirmButton: false
